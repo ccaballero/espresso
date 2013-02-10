7547ea41410d67a0661f757ebb26ca544d19c7d5
Carlos Caballero B <cijkb.j@gmail.com>
2013-02-09 21:14:30 -0400

    desarrollo inicial

---
+++ config.php
@@ -0,0 +1,35 @@
+<?php
+setlocale(LC_ALL, 'en_US.UTF8');
+
+$title = 'scesi espresso';
+$server = 'local';
+$domains = array(
+    'babel',
+    array('mælstrøm', 'maelstrom'),
+    
+    'espresso',
+    'macchiato',
+//    'cappuchino',
+//    'latte',
+//    'mochaccino',
+
+    'federica',
+//    'lebenslangerschicksalsschatz',
+//    'lemniscata',
+//    'nonchalant',
+//    'serendipia',
+//    'jornadas',
+//    'hackmeeting',
+
+//    'www',
+//    'yachay',
+
+//    'oops',
+//    'repoman',
+//    'cacti',
+
+//    'biologia',
+//    'wiki',
+//    'galeria',
+//    'ibox',
+);
---
+++ index.php
@@ -0,0 +1,28 @@
+<?php include 'config.php' ?>
+<!DOCTYPE html PUBLIC
+    "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <title><?php echo $title ?></title>
+        <link href="/images/cup.png" rel="icon" />
+        <link href="/css/style.css" media="screen" rel="stylesheet" type="text/css" />
+    </head>
+    <body>
+        <h1><?php echo $title ?></h1>
+        <div id="main">
+            <ul>
+            <?php foreach ($domains as $domain) { ?>
+                <?php $label = is_array($domain) ? $domain[0] : $domain ?>
+                <?php $url = is_array($domain) ? $domain[1] : $domain ?>
+                <li>
+                    <a target="_BLANK" href="<?php echo "http://$url.$server/" ?>">
+                        <?php echo $label ?>
+                    </a>
+                </li>
+            <?php } ?>
+            </ul>
+        </div>
+    </body>
+</html>
